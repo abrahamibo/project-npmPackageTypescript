86ab146401b89f02be0be36e1f049418fda7a523
bibo <bibo@corp.free.fr>
2020-10-17 14:46:50 +0200

    first V1

---
+++ src/__tests__/helpers.test.ts
@@ -0,0 +1,5 @@
+import { add } from "../helpers/utils";
+
+test('Add', () => {
+  expect(add(1,1)).toBe(2);
+});
---
+++ src/helpers/utils.ts
@@ -0,0 +1,6 @@
+const add = (val1: number, val2: number): any => val1 + val2;
+
+const subtract = (val1: number, val2: number) => val1 + val2;
+const multiply = (val1: number, val2: number) => val1 + val2;
+
+export { add, subtract, multiply };
